389844b25f0b957ca6eea8ab711489180a8d32d2
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-02-22 20:17:24 +0300

    Update Question 5.py

--- Question 5.py
+++ Question 5.py
@@ -1,1 +1,11 @@
+"""
+Question 5: Reverse Integer
+Write a program that takes an integer as input and returns an integer with reversed digit
+ordering.
+Examples:
+For input 500, the program should return 5.
+For input -56, the program should return -65.
+For input -90, the program should return -9.
+For input 91, the program should return 19.
 
+"""
