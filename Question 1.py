b426d1950953dae2e4d88aa838a9368b2d1a1813
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-02-22 20:20:58 +0300

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -5,4 +5,12 @@ multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5
 "FizzBuzz".
 """
 
-
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
