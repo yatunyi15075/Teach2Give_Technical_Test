7d65a05cc392dc28a684e9c54eb03a4ced62eab5
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-02-22 20:24:23 +0300

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -6,3 +6,11 @@ Examples:
 "hi"=> returns "Hi"
 "i love programming"=> returns "I Love Programming"
 """
+
+def capitalize_words(sentence):
+    return sentence.title()
+
+# Test cases
+print(capitalize_words("hi"))  # Output: "Hi"
+print(capitalize_words("i love programming")) 
+
