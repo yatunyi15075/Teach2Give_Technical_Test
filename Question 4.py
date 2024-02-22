71e7b3c5c548451b69e0beca7daea949b211fe79
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-02-22 20:14:42 +0300

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -1,1 +1,8 @@
-
+"""
+Question 4: Capitalize Words
+Write a program that accepts a string as input, capitalizes the first letter of each word in the
+string, and then returns the result string.
+Examples:
+"hi"=> returns "Hi"
+"i love programming"=> returns "I Love Programming"
+"""
