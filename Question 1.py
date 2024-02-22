8f4a83bae062f22d892499ef2470d7280bdbc3eb
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2024-02-22 20:11:17 +0300

    Update Question 1.py

--- Question 1.py
+++ Question 1.py
@@ -1,1 +1,8 @@
+"""
+Question 1: FizzBuzz
+Write a program that prints the numbers from 1 to 100. For multiples of 3, print "Fizz"; for
+multiples of 5, print "Buzz"; and for numbers that are multiples of both 3 and 5, print
+"FizzBuzz".
+"""
+
 
